0e6826a12cd99dc641e7c65788f85081542eb27d
Aniko <nahajia@gmail.com>
2026-03-18 13:51:57 +0100

    java kesz

--- alap.js
+++ alap.js
@@ -1,3 +1,8 @@
 
-
-
+function rendeles()
+{
+let etelek=parseInt(document.getElementById('etelek').value)
+if(document.getElementById('hazhoz').checked)
+    etelek+=500
+document.getElementById('kiiras').innerHTML='fizetendő: '+etelek +' ft'
+}
